0694e9f5e223568b5037d22e2de91d7a3c7c132e
jgotz <jonas.goetz01@web.de>
2023-11-03 13:44:31 +0000

    added missing interfaces

--- src/core/game.rs
+++ src/core/game.rs
@@ -7,19 +7,54 @@ pub struct Game {
 
 #[derive(Debug, Deserialize)]
 pub struct Map {
-	// width: u128,
-	// height: u128,
-	// teams: Vec<Team>,
+	pub width: u128,
+	pub height: u128,
+	pub teams: Vec<Team>,
 	pub entities: Vec<Entity>
 }
 
 #[derive(Debug, Deserialize)]
+pub struct Team {
+	pub id: u8,
+	pub name: String
+}
+
+#[derive(Debug, Deserialize)]
 pub enum Entity {
-	Core(Core)
+	Core(Core),
+	Unit(Unit),
+	Resource(Resource)
 }
 
 #[derive(Debug, Deserialize)]
 pub struct Core {
+	pub team_id: u8,
+	pub x: u128,
+	pub y: u128
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Resource {
+	pub x: u128,
+	pub y: u128,
+	pub value: u8
+}
+
+#[derive(Debug, Deserialize)]
+pub enum Unit {
+	Warrior(Warrior),
+	Worker(Worker)
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Warrior {
+	pub team_id: u8,
+	pub x: u128,
+	pub y: u128
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Worker {
 	pub team_id: u8,
 	pub x: u128,
 	pub y: u128
