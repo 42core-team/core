8f7c58550281b8b34e85db82861b41befadac2d9
jgotz <jonas.goetz01@web.de>
2023-11-03 12:50:18 +0000

    better folder structure

---
+++ src/core/game.rs
@@ -0,0 +1,26 @@
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct Game {
+	pub Map: Map
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Map {
+	// width: u128,
+	// height: u128,
+	// teams: Vec<Team>,
+	pub entities: Vec<Entity>
+}
+
+#[derive(Debug, Deserialize)]
+pub enum Entity {
+	Core(Core)
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Core {
+	pub team_id: u8,
+	pub x: u128,
+	pub y: u128
+}
---
+++ src/visualizer/test.rs
@@ -0,0 +1,30 @@
+use minifb::{Key, Window, WindowOptions};
+
+const WIDTH: usize = 800;
+const HEIGHT: usize = 600;
+
+fn main() {
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+
+    let mut window = Window::new(
+        "Test - ESC to exit",
+        WIDTH,
+        HEIGHT,
+        WindowOptions::default(),
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    // Limit to max ~30 fps update rate
+    window.limit_update_rate(Some(std::time::Duration::from_micros(33333)));
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for i in buffer.iter_mut() {
+            *i = 0; // write something more funny here!
+        }
+        window
+            .update_with_buffer(&buffer, WIDTH, HEIGHT)
+            .unwrap();
+    }
+}
