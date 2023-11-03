e3bbbb4a8957ab6f2050a3801bb8f7200e5eb6d6
jgotz <jonas.goetz01@web.de>
2023-11-03 14:29:56 +0000

    Very basic socket ping pong server working with tokyo

--- src/core/socket_connection.rs
+++ src/core/socket_connection.rs
@@ -1,47 +1,53 @@
-use std::net::{TcpListener, TcpStream};
-use std::io::{Read, Write};
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::sync::mpsc;
 
-fn handle_client(mut stream: TcpStream) {
+async fn handle_client(mut stream: tokio::net::TcpStream, sender: mpsc::Sender<tokio::net::TcpStream>) -> Result<(), Box<dyn std::error::Error>> {
     let mut buffer = [0; 1024];
+    let mut sender = sender;
 
-    // Read data from the client
-    match stream.read(&mut buffer) {
-        Ok(n) => {
-            // Convert the received data to a string
-            let received_data = String::from_utf8_lossy(&buffer[0..n]);
-            println!("Received data: {}", received_data);
-
-            // Echo the data back to the client
-            match stream.write_all(&buffer[0..n]) {
-                Ok(_) => {
-                    println!("Sent data back to client");
-                },
-                Err(e) => {
-                    eprintln!("Error sending data back to client: {}", e);
-                }
-            }
-        },
-        Err(e) => {
-            eprintln!("Error reading from client: {}", e);
+    while let Ok(n) = stream.read(&mut buffer).await {
+        if n == 0 {
+            break; // Connection closed
         }
+
+        // Convert the received data to a string
+        let received_data = String::from_utf8_lossy(&buffer[0..n]);
+        println!("Received data: {}", received_data);
+
+        // Echo the data back to the client
+        stream.write_all(&buffer[0..n]).await?;
+        println!("Sent data back to client");
     }
+
+    Ok(())
 }
 
-fn start_server() {
-	let listener = TcpListener::bind("127.0.0.1:3500")?;
+pub async fn start_server() -> Result<(), Box<dyn std::error::Error>> {
+	let listener = TcpListener::bind("127.0.0.1:3500").await?;
     println!("Server listening on 127.0.0.1:3500");
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                // Spawn a new thread to handle each incoming client
-                std::thread::spawn(|| {
-                    handle_client(stream);
-                });
+    let (sender, mut receiver) = mpsc::channel(32);
+
+    while let Ok((stream, _)) = listener.accept().await {
+        let sender = sender.clone(); // Clone the sender for each client
+
+        tokio::spawn(async move {
+            if let Err(e) = handle_client(stream, sender).await {
+                eprintln!("Error handling client: {}", e);
             }
-            Err(e) => {
-                eprintln!("Error accepting client connection: {}", e);
-            }
-        }
+        });
     }
+
+    while let Some(stream) = receiver.recv().await {
+        let sender = sender.clone(); // Clone the sender for each client
+
+        tokio::spawn(async move {
+            if let Err(e) = handle_client(stream, sender).await {
+                eprintln!("Error handling client: {}", e);
+            }
+        });
+    }
+
+    Ok(())
 }
--- src/game/main.rs
+++ src/game/main.rs
@@ -1,4 +1,10 @@
+extern crate core;
+
+use core::socket_connection::start_server;
+
 fn main() {
+	let rt = tokio::runtime::Runtime::new().unwrap();
+	let _ = rt.block_on(start_server());
 	loop {
 		println!("HELLO game");
 	}
