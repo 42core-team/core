3ac997f1aaf99cdd3b3e5e725f5a18e4a0ce3b63
jgotz <jonas.goetz01@web.de>
2023-11-06 12:12:30 +0000

    no triling characters after last }

--- src/visualizer/main.rs
+++ src/visualizer/main.rs
@@ -6,15 +6,28 @@ use core::socket_client::{connect, get_response};
 use serde_json;
 use std::error::Error;
 
+fn remove_after_last_brace(input: &str) -> String {
+    let reversed_string: String = input.chars().rev().collect();
+
+    if let Some(index) = reversed_string.find('}') {
+        let truncated_string = &reversed_string[index..].chars().rev().collect::<String>();
+        truncated_string.to_string()
+    } else {
+        input.to_string()
+    }
+}
+
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-	let mut socket = connect("127.0.0.1:3500").await?;
+	let mut socket = connect("127.0.0.1:4242").await?;
 
 	loop {
-		let json_data = get_response( &mut socket).await?;
-		println!("Received data: {}", json_data);
+		let json_data = remove_after_last_brace(get_response(&mut socket).await?.trim());
 
-		let result: Result<Game, serde_json::Error> = serde_json::from_str(json_data.as_str());
+		println!("Received data: {}", json_data.as_str().trim_end_matches(" \n\t"));
+
+		let result: Result<Game, serde_json::Error> = serde_json::from_str(json_data.as_str().trim_end_matches(" \n\t"));
 
 		println!("---------- Game data: ---------\nWidth: {:?}\nHeight: {:?}\n",
 			result.as_ref().unwrap().map.width,
