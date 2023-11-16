b504dd4d5ae3270bbd95f970813ff53c3fa6eac1
Jonas Götz <jonas.goetz01@web.de>
2023-11-16 15:44:39 +0000

    change json structure

--- src/core/socket_server.rs
+++ src/core/socket_server.rs
@@ -7,24 +7,38 @@ async fn handle_client(mut stream: tokio::net::TcpStream, sender: mpsc::Sender<t
     let mut _sender = sender;
     let json_data: &str = r#"
     {
-        "map":{
-                "width": 100000,
-                "height": 100000,
-                "teams": [
-                    { "id": 1, "name": "Team 1" },
-                    { "id": 2, "name": "Team 2" }
-                ],
-                "entities": [
-                    { "Core": { "team_id": 2, "x": 4, "y": 5 } },
-                    { "Core": { "team_id": 1, "x": 5, "y": 6 } },
-                    { "Unit": { "Warrior": {"team_id": 1, "x": 5, "y": 6 } } },
-                    { "Unit": { "Warrior": {"team_id": 2, "x": 6, "y": 7 } } },
-                    { "Unit": { "Worker": {"team_id": 1, "x": 7, "y": 6 } } },
-                    { "Unit": { "Worker": {"team_id": 2, "x": 8, "y": 7 } } },
-                    { "Resource": { "value": 1, "x": 9, "y": 6 } }
-                ]
-            }
-    }
+		"status": 0,
+		"entities": [
+			{
+				"Core": { 
+					"id": 0,
+					"team_id": 0,
+					"x": 5000,
+					"y": 6000,
+					"hp": 10000
+				}
+			},
+			{
+				"Resource": {
+					"id": 1,
+					"value": 300,
+					"x": 3000,
+					"y": 2000,
+					"hp": 4000
+				}
+			}
+		],
+		"units": [
+			{
+				"id": 2,
+				"type_id": 0,
+				"hp": 3000,
+				"x": 8000,
+				"y": 6000,
+				"team_id": 1
+			}
+		]
+	}
 "#;
 
     while let Ok(n) = stream.read(&mut buffer).await {
