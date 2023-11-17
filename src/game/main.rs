cb60273f51482f928de42796c44c70a696a7bc37
Jonas Götz <jonas.goetz01@web.de>
2023-11-17 13:13:56 +0000

    game introduction

--- src/game/main.rs
+++ src/game/main.rs
@@ -1,3 +1,13 @@
+//!
+//! ## Introduction
+//! This module is part of the CORE Project.
+//! 
+//! The Project ist a coding challenge to create a bot that can play the game CORE.
+//! 
+//! In this module the game itself gets created.
+//!
+
+
 use core::game::{Game, Team};
 
 use tokio::net::TcpListener;
