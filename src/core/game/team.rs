fd35930ab2219c17689406bfbec8fac08abc7568
Jonas Götz <jonas.goetz01@web.de>
2023-11-17 12:41:37 +0000

    better handel of the different actions

--- src/core/game/team.rs
+++ src/core/game/team.rs
@@ -2,6 +2,7 @@ use super::{action::Action, bridge::bridge, State};
 
 use tokio::{net::TcpStream, sync::mpsc::Receiver, sync::mpsc::Sender};
 
+#[allow(dead_code)] // @TODO remove when used
 pub struct Team {
     pub id: u64,
     uuid: String,
@@ -15,7 +16,7 @@ pub struct Team {
 }
 
 impl Team {
-    pub fn from_tcpStream(stream: TcpStream) -> Self {
+    pub fn from_tcp_stream(stream: TcpStream) -> Self {
         let (sender, receiver, disconnect) = bridge(stream);
 
         Team {
@@ -29,9 +30,48 @@ impl Team {
         }
     }
 
+	///
+	/// Function to handel the actions received from the client
+	/// 
+	/// a valid json to send with netcat is:
+	/// [{"Create":{"type_id":3}},{"Travel":{"id":1,"x":2,"y":3}},{"Attack":{"attacker_id":1,"target_id":2}}]
+	/// 
+	/// To uns netcat:
+	/// ```sh
+	/// nc localhost 4242
+	/// ```
+	/// then paste the json and press enter
+	/// 
+	/// You need at least two netcat instances to start a game
+	/// 
     pub fn update(&mut self) {
         while let Ok(actions) = self.receiver.try_recv() {
-            println!("TEAM action: {:?}", actions);
+            println!("TEAM {:?} send action: {:?}", self.name, actions);
+			for action in actions {
+				match action {
+					Action::Create(action) => {
+						println!("Create unit of type {:?}", action);
+						// @TODO handel create action here
+					},
+					Action::Travel(action) => {
+						println!("Travel unit of type {:?}", action);
+						// @TODO handel travel action here
+					},
+					Action::Attack(action) => {
+						println!("Attack unit of type {:?}", action);
+						// @TODO handel attack action here
+					}
+				}
+			}
         }
+		match self.disconnect.try_recv() {
+			Ok(()) => {
+				println!("Disconnect signal received");
+				// @TODO Handle disconnect here
+			},
+			Err(_) => {
+				// No disconnect signal received 
+			}
+		}
     }
 }
