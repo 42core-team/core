5cf2f5b8bc214676a641b80ce671e8f5337c7e9c
Jonas Götz <jonas.goetz01@web.de>
2023-11-17 13:13:46 +0000

    entity introduction

--- src/core/game/entity/entity.rs
+++ src/core/game/entity/entity.rs
@@ -1,3 +1,15 @@
+//!
+//! ## Introduction
+//! This module handels all the different entities that are part of the Game.
+//! 
+//! A entitiy is either a Core or a Resource
+//! 
+//! A Core is the main building of a team. It can create new entities and you need to protect it.
+//! 
+//! A Resource is a entity that can be harvested by a Unit to get more resources.
+//! 
+//!
+
 use serde::{Deserialize, Serialize};
 
 use super::{Core, Resource};
