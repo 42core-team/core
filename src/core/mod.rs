e2c8dd4361e077689462b0b1a3888f80ddbb2f05
Jonas Götz <jonas.goetz01@web.de>
2023-11-17 13:12:54 +0000

    core introduction

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,1 +1,10 @@
+//!
+//! ## Introduction
+//! This module is part of the CORE Project.
+//! 
+//! The Project ist a coding challenge to create a bot that can play the game CORE.
+//! 
+//! In this module you will find the game logic of CORE.
+//! 
+
 pub mod game;
