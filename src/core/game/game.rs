70ae7b2a027cfc1a1b087f482ea7064b559aee94
Jonas Götz <jonas.goetz01@web.de>
2023-11-20 15:46:06 +0000

    spaces to tab

--- src/core/game/game.rs
+++ src/core/game/game.rs
@@ -6,74 +6,74 @@ use super::{utils::get_ms, Entity, GameConfig, State, Team, Unit, entity::Core};
 
 #[derive(Debug)]
 pub struct Game {
-    pub teams: Vec<Team>,
-    pub config: GameConfig,
-    pub entities: Vec<Entity>,
-    pub units: Vec<Unit>,
-    pub last_tick_time: u128,
-    pub time_since_last_tick: u128,
+	pub teams: Vec<Team>,
+	pub config: GameConfig,
+	pub entities: Vec<Entity>,
+	pub units: Vec<Unit>,
+	pub last_tick_time: u128,
+	pub time_since_last_tick: u128,
 }
 
 impl Game {
-    pub fn new(teams: Vec<Team>) -> Self {
-        Game {
-            teams,
-            config: GameConfig::patch_0_1_0(),
-            entities: vec![],
-            units: vec![],
-            last_tick_time: get_ms(),
-            time_since_last_tick: 0,
-        }
-    }
+	pub fn new(teams: Vec<Team>) -> Self {
+		Game {
+			teams,
+			config: GameConfig::patch_0_1_0(),
+			entities: vec![],
+			units: vec![],
+			last_tick_time: get_ms(),
+			time_since_last_tick: 0,
+		}
+	}
 
-    pub async fn start(&mut self) {
-        loop {
-            self.wait_till_next_tick().await;
-            println!("TICK");
+	pub async fn start(&mut self) {
+		loop {
+			self.wait_till_next_tick().await;
+			println!("TICK");
 			
-            for team_index in 0..self.teams.len() {
+			for team_index in 0..self.teams.len() {
 				let team = &mut self.teams[team_index];
 				Team::update(team.id, &mut team.receiver, &mut self);
 			}
 			
 
-            self.send_state().await;
-        }
-    }
+			self.send_state().await;
+		}
+	}
 
-    async fn send_state(&mut self) {
-        let state = State::from_game(self);
-        for team in self.teams.iter_mut() {
-            let state = state.clone();
-            match team.sender.send(state).await {
+	async fn send_state(&mut self) {
+		let state = State::from_game(self);
+		for team in self.teams.iter_mut() {
+			let state = state.clone();
+			match team.sender.send(state).await {
 				Ok(_) => {}
 				Err(_) => {
 					println!("Error sending state to team");
 				}
 			}
-        }
-    }
+		}
+	}
 
-    async fn wait_till_next_tick(&mut self) {
-        let min_ms_per_tick: u128 = 3000;
+	async fn wait_till_next_tick(&mut self) {
+		let min_ms_per_tick: u128 = 3000;
 
-        loop {
-            // This is so that it always takes 1ms steps minimum
-            if get_ms() <= self.last_tick_time {
-                tokio::time::sleep(Duration::from_millis(1)).await;
-                continue;
-            }
+		loop {
+			// This is so that it always takes 1ms steps minimum
+			if get_ms() <= self.last_tick_time {
+				tokio::time::sleep(Duration::from_millis(1)).await;
+				continue;
+			}
 
-            self.time_since_last_tick = get_ms() - self.last_tick_time;
+			self.time_since_last_tick = get_ms() - self.last_tick_time;
 
-            if self.time_since_last_tick > min_ms_per_tick {
-                self.last_tick_time = self.last_tick_time + self.time_since_last_tick;
-                break;
-            }
+			if self.time_since_last_tick > min_ms_per_tick {
+				self.last_tick_time = self.last_tick_time + self.time_since_last_tick;
+				break;
+			}
 
-            tokio::time::sleep(Duration::from_millis(((min_ms_per_tick / 2) + 1) as u64)).await;
-        }
-    }
+			tokio::time::sleep(Duration::from_millis(((min_ms_per_tick / 2) + 1) as u64)).await;
+		}
+	}
 
 	pub fn generate_u64_id() -> u64 {
 		let uuid = Uuid::default();
