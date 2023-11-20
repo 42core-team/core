fe2706f7d9775783e8eecbe67a92bebb80d8e145
Jonas Götz <jonas.goetz01@web.de>
2023-11-20 15:19:13 +0000

    new unit function

--- src/core/game/unit.rs
+++ src/core/game/unit.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
+use super::{Game, GameConfig};
+
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct Unit {
     pub id: u64,
@@ -8,4 +10,17 @@ pub struct Unit {
     pub x: u64,
     pub y: u64,
     pub team_id: u64,
+}
+
+impl Unit {
+	pub fn new(team_id: u64, type_id: u64, x: u64, y: u64) -> Self {
+		Unit {
+			id: Game::generate_u64_id(),
+			type_id,
+			hp: GameConfig::get_unit_config_by_type_id(type_id).unwrap().hp,
+			x,
+			y,
+			team_id,
+		}
+	}
 }
