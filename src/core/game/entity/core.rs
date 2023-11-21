126fd604f92f872dda74efb07eceec3e655ee24f
Jonas Götz <jonas.goetz01@web.de>
2023-11-21 12:01:53 +0000

    implement core new

--- src/core/game/entity/core.rs
+++ src/core/game/entity/core.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
 
+use crate::game::{Game, GameConfig};
+
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct Core {
     pub id: u64,
@@ -7,4 +9,16 @@ pub struct Core {
     pub x: u64,
     pub y: u64,
     pub hp: u64,
+}
+
+impl Core {
+	pub fn new(team_id: u64, x: u64, y: u64) -> Self {
+		Core {
+			id: Game::generate_u64_id(),
+			team_id,
+			x,
+			y,
+			hp: GameConfig::patch_0_1_0().core_hp,
+		}
+	}
 }
