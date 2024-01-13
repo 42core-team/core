2fbfcc92a617a8dbd1b12eaef3ff44298640b2f8
Felix Brüggemann <gateway_painter_03@icloud.com>
2024-01-13 17:31:25 +0100

    added visualizer setup

--- src/visualizer/main.rs
+++ src/visualizer/main.rs
@@ -7,14 +7,42 @@
 //! In this module you will find the basic visualizer that connects to the game and visualizes the game state.
 //!
 
+use std::os::macos::raw::stat;
 
-use lib::game::action::{Action, Create};
+use lib::game::{bridge::bridge, Message, Login, State, GameConfig};
+use tokio::net::TcpStream;
 
-fn main() {
-	let a: Action = lib::game::action::Action::Create(Create { type_id: 4 });
 
-	let mut aa: Vec<Action> = vec![];
-	aa.push(a);
-	let json_string = serde_json::to_string(&aa).unwrap();
-	println!("{}", json_string);
+// IN THE HOME DIRECTORY 
+// cargo run --manifest-path core/Cargo.toml --bin game
+// cargo run --manifest-path core/Cargo.toml --bin visualizer
+
+/// PLEASE CHANGE THIS
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let mut stream = TcpStream::connect("127.0.0.1:4242").await;
+
+	if let Ok(s) = stream {
+		let (sender, mut reciever, disconnect) = bridge(s);
+
+		let config: GameConfig = GameConfig::patch_0_1_0();
+		sender.send(Message::Login(Login{id: 42})).await;
+		loop {
+			if let Some(m) = reciever.recv().await {
+				if let Message::State(x) = m {
+					print(&config, x).await;
+				}
+			}
+		}
+		
+	}
+	
+    Ok(())
+}
+
+
+async fn print(config: &GameConfig, state: State) {
+	// DEFENETLY REMOVE THIS AT THE END PLEASE!!!!!!!
+	println!("XXX {:?}", state);
+	
 }
