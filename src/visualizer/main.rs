f9175bbaf76a1fb046720e31ab8872f53b55e169
felix.brgm <gateway_painter_03@icloud.com>
2024-01-13 19:33:18 +0000

    removed shit

--- src/visualizer/main.rs
+++ src/visualizer/main.rs
@@ -1,22 +1,18 @@
 //!
 //! ## Introduction
 //! This module is part of the CORE Project.
-//! 
+//!
 //! The Project ist a coding challenge to create a bot that can play the game CORE.
-//! 
+//!
 //! In this module you will find the basic visualizer that connects to the game and visualizes the game state.
 //!
 
-use std::os::macos::raw::stat;
-
-use lib::game::{bridge::bridge, Message, Login, State, GameConfig};
+use lib::game::{bridge::bridge, GameConfig, Login, Message, State};
 use tokio::net::TcpStream;
 
-use crossterm::{execute, cursor, Result};
 use std::io::{stdout, Write};
 
-
-// IN THE HOME DIRECTORY 
+// IN THE HOME DIRECTORY
 // cargo run --manifest-path core/Cargo.toml --bin game
 // cargo run --manifest-path core/Cargo.toml --bin visualizer
 
@@ -32,29 +28,26 @@ use std::io::{stdout, Write};
 /// PLEASE CHANGE THIS
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-	let mut stream = TcpStream::connect("127.0.0.1:4242").await;
+    let mut stream = TcpStream::connect("127.0.0.1:4242").await;
 
-	if let Ok(s) = stream {
-		let (sender, mut reciever, disconnect) = bridge(s);
+    if let Ok(s) = stream {
+        let (sender, mut reciever, disconnect) = bridge(s);
 
-		let config: GameConfig = GameConfig::patch_0_1_0(); //needs to be made dynamic after all important shit is done!!!
-		sender.send(Message::Login(Login{id: 42})).await;
-		loop {
-			if let Some(m) = reciever.recv().await {
-				if let Message::State(x) = m {
-					print(&config, x).await;
-				}
-			}
-		}
-		
-	}
-	
-	Ok(())
+        let config: GameConfig = GameConfig::patch_0_1_0(); //needs to be made dynamic after all important shit is done!!!
+        sender.send(Message::Login(Login { id: 42 })).await;
+        loop {
+            if let Some(m) = reciever.recv().await {
+                if let Message::State(x) = m {
+                    print(&config, x).await;
+                }
+            }
+        }
+    }
+
+    Ok(())
 }
 
-
 async fn print(config: &GameConfig, state: State) {
-	// DEFINETLY REMOVE THIS AT THE END PLEASE!!!!!!!
-	println!("XXX {:?}", state);
-	
+    // DEFINETLY REMOVE THIS AT THE END PLEASE!!!!!!!
+    println!("XXX {:?}", state);
 }
