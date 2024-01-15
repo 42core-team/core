51a1fe205d876b38142540886105cd4216869e00
LeondavidZipp <leondavidzipp@gmx.de>
2024-01-15 13:14:42 +0000

    jonas fixed my shit : get_character, clear_map, etc...

--- src/visualizer/main.rs
+++ src/visualizer/main.rs
@@ -7,54 +7,105 @@
 //! In this module you will find the basic visualizer that connects to the game and visualizes the game state.
 //!
 
-use std::os::macos::raw::stat;
+// use std::os::macos::raw::stat;
 
-use lib::game::{bridge::bridge, Message, Login, State, GameConfig};
+use lib::game::{bridge::bridge, Message, Login, State, GameConfig, Game, state};
 use tokio::net::TcpStream;
 
-use crossterm::{execute, cursor, Result};
+use crossterm::{execute, cursor};
 use std::io::{stdout, Write};
-
+// use crossterm::execute::{execute, stdout};
+// use crossterm::cursor;
+// use std::io::Write;
 
 // IN THE HOME DIRECTORY 
 // cargo run --manifest-path core/Cargo.toml --bin game
-// cargo run --manifest-path core/Cargo.toml --bin visualizer
+// cargo run --manifest-path Cargo.toml --bin visualizer
 
-// fn clear_map () {
-// 	let mut stdout = stdout();
-// 	execute!(stdout, cursor::Hide)?;
-// 	execute!(stdout, cursor::MoveTo(0, 0))?;
-// 	execute!(stdout, cursor::Show)?;
-// }
+fn clear_map () {
+	println!("{}{}", cursor::MoveTo(0, 0), cursor::Hide);
+}
+
+fn get_character(x: u64, y: u64, state: State) {
+	state.cores.iter().for_each(|core| {
+		if core.x == x && core.y == y {
+			print!("C");
+		}
+	});
+	state.units.iter().for_each(|unit| {
+		if unit.x == x && unit.y == y {
+			if unit.type_id == GameConfig::patch_0_1_0().units[0].type_id{
+				print!("U");
+			} else if unit.type_id == GameConfig::patch_0_1_0().units[1].type_id {
+				print!("U");
+			} else {
+				println!("Unknown unit type!");
+			}
+		}
+	});
+	state.resources.iter().for_each(|resource| {
+		if resource.x == x && resource.y == y {
+			print!("R");
+		}
+	});
+	print!("D");
+}
+
+fn show_map(state: State, config: GameConfig){
+	clear_map();
+	for y in 0..config.height {
+		for x in 0..config.width {
+			get_character(x, y, state.clone());
+		}
+	}
+}
 
 // fn print_map
 
 /// PLEASE CHANGE THIS
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+	loop {
+		clear_map();
+	}
 	let mut stream = TcpStream::connect("127.0.0.1:4242").await;
 
 	if let Ok(s) = stream {
 		let (sender, mut reciever, disconnect) = bridge(s);
+		let gameconfig: GameConfig;
 
 		let config: GameConfig = GameConfig::patch_0_1_0(); //needs to be made dynamic after all important shit is done!!!
 		sender.send(Message::Login(Login{id: 42})).await;
+		if let Some(m) = reciever.recv().await {
+			match m {
+				Message::GameConfig(config) => {
+					gameconfig = config;
+				},
+				_ => {
+					println!("First message was not a gameconfig!");
+				},
+			}
+		}
 		loop {
 			if let Some(m) = reciever.recv().await {
-				if let Message::State(x) = m {
-					print(&config, x).await;
+				match m {
+					Message::State(state) => {
+						show_map(state, gameconfig);
+					},
+					_ => {
+						println!("unexpected message type!");
+					},
 				}
 			}
 		}
 		
 	}
-	
 	Ok(())
 }
 
 
-async fn print(config: &GameConfig, state: State) {
-	// DEFINETLY REMOVE THIS AT THE END PLEASE!!!!!!!
-	println!("XXX {:?}", state);
+// async fn print(config: &GameConfig, state: State) {
+// 	// DEFINETLY REMOVE THIS AT THE END PLEASE!!!!!!!
+// 	println!("XXX {:?}", state);
 	
-}
+// }
