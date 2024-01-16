8094eaa9b385991dff9210738c4b72aca306acb4
Jonas Götz <jonas.goetz01@web.de>
2024-01-16 11:46:02 +0000

    if test no logs

--- src/core/game/log/log.rs
+++ src/core/game/log/log.rs
@@ -6,8 +6,9 @@ use chrono::Local;
 use super::LogOptions;
 
 pub struct Logger {}
-
+#[cfg(not(test))]
 impl Logger {
+    #[cfg(not(test))]
     pub fn new() -> Logger {
         // Create logs folder if it doesn't exist
         fs::create_dir_all("logs").unwrap();
@@ -15,13 +16,15 @@ impl Logger {
         // Create old-logs folder if it doesn't exist
         fs::create_dir_all("old-logs").unwrap();
 
-        //if logs folder is not empty, move all files to old-logs folder
+        // Move existing log files to old-logs folder
         let paths = fs::read_dir("logs").unwrap();
         for path in paths {
             let path = path.unwrap().path();
             let file_name = path.file_name().unwrap().to_str().unwrap();
             let old_log_file_path = format!("old-logs/{}", file_name);
-            fs::rename(&path, &old_log_file_path).unwrap();
+            if fs::metadata(&path).is_ok() && fs::metadata(&old_log_file_path).is_ok() {
+                fs::rename(&path, &old_log_file_path).unwrap();
+            }
         }
 
         // Create a new log file for each log option if it doesn't exist
@@ -57,6 +60,14 @@ impl Logger {
     }
 }
 
+#[cfg(not(test))]
 pub fn log(log_options: LogOptions, message: &str) {
     Logger::log(log_options, message.to_string());
+}
+
+#[cfg(test)]
+pub fn log(log_options: LogOptions, message: &str) {
+    // Only the log function is implemented for tests
+    // You might want to replace this with an implementation that suits your testing needs
+    println!("Test log: {:?} - {}", log_options, message);
 }
